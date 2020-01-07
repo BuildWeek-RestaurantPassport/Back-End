325af9ecb24bfa270f51636b8bb1e855e7334e7b
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2020-01-06 22:07:29 -0600

    Completed set up in knex file for Heruko deployment.

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,29 @@
+require('dotenv').config();
+
+const pg = require("pg")
+pg.defaults.ssl = true;
+
 module.exports = {
   development: {
     client: 'sqlite3',
-    connection: { filename: './database/auth.db3' },
+    //This was updated with DB for Heroku
+    connection: process.env.DB_URL,
     useNullAsDefault: true,
     migrations: {
       directory: './database/migrations',
       tableName: 'dbmigrations',
     },
     seeds: { directory: './database/seeds' },
+  },
+
+
+//This part is done w POSTGRES
+  production: {
+    client: 'pg',
+    connection: process.env.DB_URL,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: { directory: './data/seeds' },
   },
 };
