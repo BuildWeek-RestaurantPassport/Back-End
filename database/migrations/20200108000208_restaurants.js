14e436f244cee4f20375c8c6405cc4f48fce618c
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2020-01-08 21:06:36 -0600

    Completed Restaurant Migration Table.

--- database/migrations/20200108000208_restaurants.js
+++ database/migrations/20200108000208_restaurants.js
@@ -1,8 +1,23 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable("rests", tbl => {
+        tbl.increments();
+        tbl.string("id", 128).notNullable().unique();
+        tbl.string("rest_name", 128).notNullable().unique();
+        tbl.string("img_url", 128).notNullable().unique();
+        tbl.string("url", 128).notNullable().unique();
+        tbl.string("phone_num", 128).notNullable().unique();
+        tbl.integer("rating", 128).notNullable().unique();
+        tbl.string("address", 128).notNullable().unique();
+        tbl.string("city", 128).notNullable().unique();
+        tbl.string("zip", 128).notNullable().unique();
+        tbl.string("country", 128).notNullable().unique();
+        tbl.string("state", 128).notNullable().unique();
+        tbl.string("price", 128).notNullable().unique();
+
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists("rests")
 };
